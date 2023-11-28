b5bfec15245ee1212ee515941c1b17f3e88a6237
José Iêdo <iedoduarte1@gmail.com>
2023-11-28 20:23:57 -0300

    add main pack plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,12 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.pack.rust"},
+  { import = "astrocommunity.pack.java"},
+  { import = "astrocommunity.pack.kotlin"},
+  { import = "astrocommunity.pack.docker"},
+  { import = "astrocommunity.pack.yaml"},
+  { import = "astrocommunity.pack.json"},
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
